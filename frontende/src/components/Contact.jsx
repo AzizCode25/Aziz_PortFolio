4c804ce59bf567610aa7ae904a50abab422ccbb7
TheCodeGym <azizcode92@gmail.com>
2025-08-11 02:49:21 +0200

    Api

--- frontende/src/components/Contact.jsx
+++ frontende/src/components/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
 
     try {
       const { data } = await axios.post(
-        'https://aziz-portfolio-api.onrender.com/api/v1/contact/send',
+        'https://aziz-portfolio-z6cs.onrender.com/api/v1/contact/send',
         formData,
         {
           withCredentials: true,
