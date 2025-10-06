05ac78726133be00c57ab7e61fd485ecae7e2980
TheCodeGym <azizcode92@gmail.com>
2025-10-06 03:09:36 +0200

    da

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import app from './app.js'
 
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server listening at port ${process.env.PORT}`)
-})
+// app.listen(process.env.PORT, ()=>{
+//     console.log(`Server listening at port ${process.env.PORT}`)
+// })
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
