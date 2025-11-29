ab6951b5a1f5ea01f1c550550857b06d9efd9f64
TheCodeGym <azizcode92@gmail.com>
2025-11-29 18:11:08 +0100

    fix die projetk image in Obj-Top

--- frontende/src/components/Project.jsx
+++ frontende/src/components/Project.jsx
@@ -56,7 +56,7 @@ const Project = () => {
                     <img
                       src={project.image}
                       alt={project.name}
-                      className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
+                      className="w-full h-full object-cover transition-transform object-top duration-700 group-hover:scale-110"
                       loading="lazy"
                     />
 
